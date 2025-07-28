f7df10619a0017f67c566471fc5967191f23a23d
Aborrol <aborrol@mail.ru>
2025-07-28 12:21:34 +0300

    Add automated version update script and update all platform packages to 2.6.2-ab9

---
+++ scripts/update-versions.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+
+// Read main package.json
+const mainPackage = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
+const version = mainPackage.version
+
+console.log(`Updating all platform packages to version: ${version}`)
+
+// Find all platform package.json files
+const platformPackages = glob.sync('npm/*/package.json')
+
+platformPackages.forEach((filePath) => {
+  console.log(`Updating ${filePath}...`)
+
+  const packageJson = JSON.parse(fs.readFileSync(filePath, 'utf8'))
+
+  // Update version
+  packageJson.version = version
+
+  // Update repository
+  packageJson.repository = 'https://github.com/Aborrol/resvg-js'
+
+  // Write back
+  fs.writeFileSync(filePath, JSON.stringify(packageJson, null, 2) + '\n')
+})
+
+console.log(`✅ Updated ${platformPackages.length} platform packages to version ${version}`)
